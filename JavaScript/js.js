a57ace4bd38e7e5954d711ff8a9f3b298f041773
ru13y <ru13y051@gmail.com>
2022-09-12 17:11:21 +0430

    Lazy Mode

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -40,3 +40,49 @@ function scrollHeader(){
   if(this.scrollY >=50)header.classList.add('scroll-header');else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll',scrollHeader)
+
+
+//TODO: Click Login
+const nav_btn=document.querySelector('.nav__btns')
+const login__close=document.querySelector('.login__close')
+const login=document.querySelector('.login')
+nav_btn.addEventListener('click',function(){
+  login.style.right=0
+  
+})
+login__close.addEventListener('click',function(){
+  login.style.right='-100%'
+})
+
+
+//TODO:Lazy Loading 
+const dataScr=document.querySelector('.section__title')
+console.log(dataScr);
+const srcFunc=function(elm,observer){
+const [srcPic]=elm;
+  if(!srcPic.isIntersecting)return
+  const dataPic=document.querySelectorAll('img[data-src]')
+dataPic.forEach(e=>{
+
+  e.src=e.dataset.src;
+  e.addEventListener('load',function(){
+    console.log(e);
+    e.classList.remove('lazy-img')
+    })
+})
+observer.unobserve(srcPic.target);
+}
+
+
+
+
+
+const dataSrcObserver=new IntersectionObserver(srcFunc,{
+root:null,
+threshold:0,
+rootMargin:'100px'
+})
+
+
+
+dataSrcObserver.observe(dataScr)
